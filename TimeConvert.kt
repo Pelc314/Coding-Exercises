938a7a03eee19f4453628c60625e6d05d51b5e51
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2022-04-16 13:35:06 +0200

    Update TimeConvert.kt

--- TimeConvert.kt
+++ TimeConvert.kt
@@ -1,2 +1,14 @@
 package src
+fun main() {
+    val input = 6271
+    var output = timeConvert(input)
+    println(output)
+}
 
+fun timeConvert(number: Int): String {
+    var hours = number / 60
+    var minutes = number % 60
+    var hour = "${hours.toString()}:${minutes.toString()}"
+
+    return hour
+}
